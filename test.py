58a9526b11cae01154bfdaa2edc71cba8315bcaa
Bregt van der Lugt <bregtvdl@gmail.com>
2025-02-18 11:06:16 +0100

    test2

--- test.py
+++ test.py
@@ -8,4 +8,8 @@ def test1():
 def test2():
     print("eloo2")
     print("change from test")
+<<<<<<< HEAD
     print("Nu op dezeflde plek -- vanaf master")
+=======
+    print("vanaf test")
+>>>>>>> a547cc4 (test2)
