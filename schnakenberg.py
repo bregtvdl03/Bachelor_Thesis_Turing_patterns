b7a97dbe4b46b5edfceb932ebf828e361defa992
Bregt van der Lugt <bregtvdl@gmail.com>
2025-10-01 14:32:19 +0200

    PATTEEEERNSS

--- schnakenberg.py
+++ schnakenberg.py
@@ -13,9 +13,9 @@ from dolfinx.fem.petsc import assemble_vector, assemble_matrix, create_vector
 OUT_FILE = "out_schnakenberg/schakenberg.gif"
 
 Du = 1.0 # Diffusion coef for u
-Dv = 30.0 # Diffusion coef for v
-Pu = 0.2 # Production coef for u
-Pv = 0.66 # Production coef for v
+Dv = 40.0 # Diffusion coef for v
+Pu = 0.1 # Production coef for u
+Pv = 0.9 # Production coef for v
 gamma = 1.0 # Time scaling
 
 uniform_steady_state_u = Pu + Pv
@@ -32,25 +32,25 @@ def initial_condition_v(x):
     # return [uniform_steady_state_v] * x.shape[1]
 
 t = 0.0
-T = 1.0
-num_steps = 128
+T = 100.0
+num_steps = 1280
 dt = T / num_steps
 
 nx, ny = 128, 128
 
-# domain = mesh.create_rectangle(
-#     comm=MPI.COMM_WORLD,
-#     points=[[-2.0, -2.0], [2.0, 2.0]],
-#     n=[nx, ny],
-#     cell_type=mesh.CellType.triangle
-# )
-
-domain = mesh.create_unit_square(
+domain = mesh.create_rectangle(
     comm=MPI.COMM_WORLD,
-    nx=nx,
-    ny=ny,
+    points=[[-128.0, -128.0], [128.0, 128.0]],
+    n=[nx, ny],
     cell_type=mesh.CellType.triangle
 )
+
+# domain = mesh.create_unit_square(
+#     comm=MPI.COMM_WORLD,
+#     nx=nx,
+#     ny=ny,
+#     cell_type=mesh.CellType.triangle
+# )
 
 el_u = basix.ufl.element("Lagrange", basix.CellType.triangle, 1)
 el_v = basix.ufl.element("Lagrange", basix.CellType.triangle, 1)
@@ -103,7 +103,7 @@ u_grid = pyvista.UnstructuredGrid(*plot.vtk_mesh(V0))
 v_grid = pyvista.UnstructuredGrid(*plot.vtk_mesh(V1))
 
 plotter = pyvista.Plotter()
-plotter.open_gif(OUT_FILE, fps=20)
+plotter.open_gif(OUT_FILE, fps=40)
 plotter.show_grid(
     font_size = 15,
     font_family = "times",
@@ -117,14 +117,15 @@ v_grid.point_data["vh"] = v_n.x.array[mapv]
 u_graph = u_grid.warp_by_scalar("uh", factor=1)
 v_graph = v_grid.warp_by_scalar("vh", factor=1)
 
-blues = mpl.colormaps.get_cmap("Blues").resampled(20)
-ylorrd = mpl.colormaps.get_cmap("YlOrRd").resampled(20)
-colorwidth = 0.05
+blues = mpl.colormaps.get_cmap("Blues").resampled(32)
+ylorrd = mpl.colormaps.get_cmap("YlOrRd").resampled(32)
+colorwidth = 0.005
 
 plotter.add_mesh(
     u_graph,
     show_edges=False,
     lighting=False,
+    opacity=0.8,
     cmap=blues,
     clim=[uniform_steady_state_u - colorwidth, uniform_steady_state_u + colorwidth],
     scalar_bar_args={
@@ -139,7 +140,6 @@ plotter.add_mesh(
     show_edges=False,
     lighting=False,
     cmap=ylorrd,
-    opacity=0.8,
     clim=[uniform_steady_state_v - colorwidth, uniform_steady_state_v + colorwidth],
     scalar_bar_args={
         "font_family": "times",
